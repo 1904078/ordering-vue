ca47caf6151632019177110085f9045618f36be7
1670691647@qq.com <167091647@qq.com>
2020-09-04 22:56:49 +0800

    添加node配置及node路由设置

---
+++ server/routes/product.js
@@ -0,0 +1,21 @@
+var express=require('express')
+var router=express.Router();
+var mongoose=require('mongoose');
+var product=require('../models/product');
+
+mongoose.connect('mongodb://127.0.0.1:27017/ordering');
+mongoose.connection.on("connected",function(){
+    console.log("MongoDB connected success")
+});
+mongoose.connection.on("error",function(){
+    console.log("MongoDB connected fail")
+});
+mongoose.connection.on("disconnected",function(){
+    console.log("MongoDB  disconnected")
+});
+
+router.get("/",function(req,res,next){
+    res.send('hello,goods list')
+})
+
+module.exports = router;
